e1d8b3388869408c45e1ec6d8e0a7fe39ccdda55
dwebbjcg <63323184+dwebbjcg@users.noreply.github.com>
2020-05-26 21:44:54 -0400

    Attempted

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: object[] = [
+    { items: "Habitat dome" },
+    { items: "Drone" },
+    { items: "Food containers" },
+    { items: "Oxygen Tanks" },
+  ]
+
+  itemBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(newItems: string) {
+    this.equipment.push({ items: newItems });
+  }
+
+  remove(item: object) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(item: object) {
+    this.itemBeingEdited = item;
+  }
+
+  save(items: string, item: object) {
+    item['items'] = items;
+    this.itemBeingEdited = null;
   }
 
 }
