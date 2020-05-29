418bc25f1fd3dc879efb9340192ae99080eb66ca
dwebbjcg <63323184+dwebbjcg@users.noreply.github.com>
2020-05-28 21:18:32 -0400

    first commit Ang studio

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,40 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
+  equipmentItems: object[] = [
+    { name: 'Duct Tape', mass: 0.5 },
+    { name: 'Space Camera', mass: 20 },
+    { name: 'Food', mass: 150 },
+    { name: 'Oxygen Tanks', mass: 400 },
+    { name: 'AE-35 Unit', mass: 5 },
+    { name: 'ISS Supplies', mass: 800 },
+    { name: 'Water', mass: 250 },
+    { name: 'Satellite', mass: 1200 },
+    { name: 'R2 Unit', mass: 32 }
+  ];
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() { }
+  ngOnInit() { }
 
-   addItem(equipment: {name, mass}): boolean{
-     this.cargoHold.push(equipment);
-     this.cargoMass+=equipment.mass;
-     if(this.maximumAllowedMass-this.cargoMass<=200){
-       return true;
-     }
-     return false;
-   }
+  addItem(equipment: { name, mass }): boolean {
+    this.cargoHold.push(equipment);
+    this.cargoMass += equipment.mass;
+    if (this.maximumAllowedMass - this.cargoMass <= 200) {
+      return true;
+    }
+    return false;
+  }
 
-   emptyHold(){
-     this.cargoMass=0;
-     this.cargoHold=[];
-   }
+  removeItem(equipment: { name, mass }): boolean {
+    this.cargoHold.splice(0, 1);
+    this.cargoMass = this.cargoMass - equipment.mass;
+    if (this.maximumAllowedMass + this.cargoMass <= 200) {
+      return true;
+    }
+    return false;
+  }
 
-   // Code your addItem function here:
-   
+  emptyHold() {
+    this.cargoMass = 0;
+    this.cargoHold = [];
+  }
+
+  // Code your addItem function here:
+
 }
